45e6bf24ffc8bc5374470ec4374a3b86e8419a94
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-02-04 14:40:14 -0500

    spelling error db to database

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
